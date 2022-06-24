7bc49460c0796b81624eb16fbd50851c5e5b2edc
Andrey Kovalenko <ak@taucode.com>
2022-06-24 18:41:57 +0300

    Initial release.

---
+++ test/TauCode.Algorithms.Abstractions.Tests/DummyFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TauCode.Algorithms.Abstractions.Tests
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void DummyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Pass("Added for pipeline.");
+        }
+    }
+}
